ad663da2ba6631cea88ba07e460ecb681b9e2800
Komyshev Vitaliy <55884441+nicethatcher@users.noreply.github.com>
2020-10-02 01:38:14 +0300

    Add files via upload

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Material[] material;
+    public Renderer rend;
+    public int i = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // по клику ЛКМ
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (i > 0)
+            {
+                rend.sharedMaterial = material[2];
+            }
+            else
+            {
+                rend.sharedMaterial = material[0];
+            };
+            i = 0 - i;
+        }
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public float Radius = 10;
+    public GameObject gameObject;
+
+    void Start()
+    {
+        int i = 0;
+        for ( ; ; )
+        {
+            int x = Random.Range(-10, 10);
+            int z = Random.Range(-10, 10);
+            if ((x*x + z*z) <= Radius*Radius)
+            {
+                i = i + 1;
+                Instantiate(gameObject, new Vector3(x, 0, z), Quaternion.identity);
+                if (i > 9) break;
+            }
+        }
+    }
+}
